7f83c9bdac74913ae691536ef2aa5920ddecbaac
NightProg <tonio.barbier@gmail.com>
2024-04-01 08:44:40 +0200

    add error

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -1,3 +1,4 @@
+use ariadne::{Label, Report, ReportKind, Source};
 use crate::ast::Span;
 
 #[derive(Debug, PartialEq, Clone)]
@@ -18,4 +19,60 @@ impl Error {
     pub fn new(kind: ErrorKind, span: Span) -> Error {
         Error { kind, span }
     }
+
+    pub fn report(&self, filename: &str) {
+        let source = &std::fs::read_to_string(filename).unwrap();
+        let mut report = Report::build(ReportKind::Error, filename, self.span.get_line_number(source));
+        match &self.kind {
+            ErrorKind::UnexpectedToken { expected, found } => {
+                report = report.with_code("unexpected-token")
+                    .with_message(format!("Unexpected token found at line {}",
+                                          found.get_line_number(source)))
+                    .with_label(Label::new(
+                            (filename, found.start..found.end)
+                        )
+                        .with_message(format!("Expected {}", expected))
+                    )
+                ;
+
+            },
+            ErrorKind::UnexpectedEOF { expected } => {
+                report = report.with_code("unexpected-eof")
+                    .with_message("Unexpected end of input")
+                    .with_label(Label::new(
+                            (filename, self.span.start..self.span.end)
+                        )
+                        .with_message(format!("Expected {}", expected))
+                    )
+                ;
+            },
+            ErrorKind::UnexpectedEndOfInput => {
+                report = report.with_code("unexpected-end-of-input")
+                    .with_message("Unexpected end of input")
+                    .with_label(Label::new(
+                            (filename, self.span.start..self.span.end)
+                        )
+                        .with_message("Expected more input")
+                    )
+                ;
+            },
+            ErrorKind::UnexpectedTokenInPattern { found } => {
+                report = report.with_code("unexpected-token-in-pattern")
+                    .with_message(format!("Unexpected token found in pattern at line {}",
+                                          found.get_line_number(source)))
+                    .with_label(Label::new(
+                            (filename, found.start..found.end)
+                        )
+                        .with_message("Unexpected token in pattern")
+                    )
+                ;
+            }
+        }
+        
+        report
+            .finish()
+            .print((filename, Source::from(source)))
+            .unwrap();
+    }
 }
+
